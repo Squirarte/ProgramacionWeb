9aac40f6102756731c031178affe5fa109ccd5d0
Squirarte <santiagoquirartecordero@gmail.com>
2020-10-29 22:45:31 -0600

    Preuba

---
+++ Codigo/js/script.js
@@ -0,0 +1,18 @@
+console.log(5);
+console.log("Hola Mundo!");
+console.log("Hola Mundo!".toUpperCase());
+console.log("Hola Mundo!".replace('Hola','Adios'));
+console.log("Hola Mundo!".length);
+console.log(234.456);
+console.log(30+5);
+console.log(30-5);
+//Tipos de datos: Numericos, String, Boolean, null, undefined, symbol, Object.
+
+//Variables 
+var x =10;
+x = "Hola";
+let y = 10; //es mejor utilizar let
+const vacio = false; //Variable que no cambia de tipo de dato
+
+let res = x.repeat(4);
+ console.log(res);
