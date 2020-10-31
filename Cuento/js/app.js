d24bd6db67a0e382ce9c47ea8b2f54e9ec3409ae
Squirarte <santiagoquirartecordero@gmail.com>
2020-10-30 23:07:36 -0600

    cuento

---
+++ Cuento/js/app.js
@@ -0,0 +1,113 @@
+particlesJS(
+     {
+  "particles": {
+    "number": {
+      "value": 10,
+      "density": {
+        "enable": false,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#1b1e34"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "https://cdn.discordapp.com/attachments/764240891870576670/771607458304098334/Met.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.5,
+        "sync": true
+      }
+    },
+    "size": {
+      "value": 120,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 20,
+        "size_min": 80,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 400,
+      "color": "#ffffff",
+      "opacity": 6,
+      "width": 6
+    },
+    "move": {
+      "enable": true,
+      "speed": 15,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "remove"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 1
+      },
+      "remove": {
+        "particles_nb": 1
+      }
+    }
+  },
+  "retina_detect": true
+}
+
+	);
